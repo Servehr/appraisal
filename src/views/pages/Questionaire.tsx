10ccdbf6b119e8907d4080a747d75f6bb7f0ebc6
Hammed <hjimoh@bcodestech.com>
2023-08-30 12:23:53 +0100

    Resolved Navigation

--- src/views/pages/Questionaire.tsx
+++ src/views/pages/Questionaire.tsx
@@ -60,41 +60,46 @@ export const Questionaire = () =>
               </div>
   
               <section className="section dashboard container">
-                  <div className="container">
-                      <div className="row">
-  
-                          {
-                              questionaire.questions.map((rsur: any, index: number) => {
-                                  let clax:string = `bi bi-${rsur.icon} col-md-1 float-end`
-                                  return <div className="col-6 col-md-3" style={{paddingRight: '8px', paddingLeft: '8px', position: 'relative'}} key={index}>
-                                              <a href='#/questions'>
-                                                  <div className="card info-card revenue-card flex">
-                                                      <div className="card-body">
-                                                          <h4 className="card-title" style={{marginBottom:'5px', fontSize: '14px'}}>{rsur.text}</h4>
-                                                          {
+              <div className="container">
+                    <div className="row mb-4">
+
+                        {
+                            questionaire.questions.map((rsur: any, index: number) => {
+                                let clax:string = `bi bi-${rsur.icon} col-md-1 float-end`
+                                return <div className="col-6 col-md-3 mb-3" style={{paddingRight: '8px', paddingLeft: '8px', position: 'relative'}} key={index}>
+                                            <div className='d-flex row'>
+                                                
+                                            <a href='files' className='col-11 mx-2'>
+                                                <div className="card info-card revenue-card flex" style={{marginBottom: '0px'}}>
+                                                    <div className="card-body">
+                                                        <h4 className="card-title" style={{marginBottom:'5px', fontSize: '14px'}}>{rsur.text}</h4>
+                                                        <div className="d-flex justify-content-between">
+                                                            <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                                                <i className="bi bi-folder2-open" style={{fontSize: '22px'}}></i>
+                                                            </div>
+                                                            <div className="p-0">
+                                                                <div className='col-12' style={{fontSize: '27px', marginTop: '-15px'}}>{rsur.files}</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            
+                                            <div className='col-11 p-1' style={{bottom: 0, left: 0}}>
+                                                            {
                                                                 questionaire.flyOut.map((action, index) => {
                                                                     let clax:string = `bi bi-${action.icon} col-md-1 float-end`
-                                                                    return <i className={clax} data-bs-toggle="modal" data-bs-target={action.target} style={{fontSize: '15px', cursor: 'pointer', 'marginLeft': '12px', top: 0, right: 0, zIndex: '1000px'}}></i>
+                                                                    return <i className={clax} data-bs-toggle="modal" data-bs-target={action.target} style={{fontSize: '15px', cursor: 'pointer', 'marginLeft': '30px'}}></i>
                                                                 })
-                                                          }
-                  
-                                                          <div className="d-flex justify-content-between">
-                                                              <div className="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                                                  <i className="bi bi-folder2-open" style={{fontSize: '22px'}}></i>
-                                                              </div>
-                                                              <div className="ps-3 mt-3">
-                                                                  <div className='col-12' style={{fontSize: '27px'}}>{rsur.files}</div>
-                                                              </div>
-                                                          </div>
-                                                      </div>
-                                                  </div>
-                                              </a>
-                                          </div>
-                              })
-                          }
-  
-                      </div>
-                  </div>
+                                                            }
+                                                        </div>
+                                            </div>
+                                        </div>
+                            })
+                        }
+
+                    </div>
+                </div>
               </section>
   
               <Pagination />
